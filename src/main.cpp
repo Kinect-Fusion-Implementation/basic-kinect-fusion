d3488a8ab81eefdc6fc19ecc6e6adc1be6866f4b
Dominik Voigt <dominik.voigt@tum.de>
2023-07-30 22:56:21 +0200

    Add time measurements

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ int icp_accuracy_test()
         float normal_diff_threshold = 0.002;
         std::vector<int> iterations_per_level = {10, 10, 10};
         ICPOptimizer optimizer(sensor.getDepthIntrinsics(), sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), vertex_diff_threshold, normal_diff_threshold, iterations_per_level, 0.5f);
-        
+
         Matrix4f prevFrameToGlobal = Matrix4f::Identity();
         int idx = 0;
         Matrix4f trajectoryOffset;
@@ -83,7 +83,7 @@ int icp_accuracy_test()
                         }
                 }
 
-                //RaycastImage raycast = grid.raycastVoxelGrid(sensor.getTrajectory() * trajectoryOffset, sensor.getDepthIntrinsics());
+                // RaycastImage raycast = grid.raycastVoxelGrid(sensor.getTrajectory() * trajectoryOffset, sensor.getDepthIntrinsics());
                 RaycastImage raycast = grid.raycastVoxelGrid(prevFrameToGlobal.inverse(), sensor.getDepthIntrinsics());
                 if (idx % 100 == 0)
                 {
@@ -97,24 +97,24 @@ int icp_accuracy_test()
                 estPose = optimizer.optimize(pyramid, raycast.m_vertexMapGPU, raycast.m_normalMapGPU, prevFrameToGlobal);
                 std::cout << "Ground Truth: " << std::endl
                           << gt_pose << std::endl;
-                //std::cout << "Determinant: " << gt_pose.determinant() << std::endl;
+                // std::cout << "Determinant: " << gt_pose.determinant() << std::endl;
                 std::cout << "Estimated: " << std::endl
                           << estPose << std::endl;
                 std::cout << "Determinant estimate: " << estPose.determinant() << std::endl;
-                std::cout << "Norm: " << (estPose-gt_pose).norm() << std::endl;
+                std::cout << "Norm: " << (estPose - gt_pose).norm() << std::endl;
                 // Use estimated pose as prevPose for next frame
                 prevFrameToGlobal = estPose;
-                //grid.updateTSDF(sensor.getTrajectory() * trajectoryOffset, sensor.getDepthIntrinsics(), depth, sensor.getDepthImageWidth(), sensor.getDepthImageHeight());
+                // grid.updateTSDF(sensor.getTrajectory() * trajectoryOffset, sensor.getDepthIntrinsics(), depth, sensor.getDepthImageWidth(), sensor.getDepthImageHeight());
                 grid.updateTSDF(estPose.inverse(), sensor.getDepthIntrinsics(), depth, sensor.getDepthImageWidth(), sensor.getDepthImageHeight());
         }
         run_marching_cubes(grid, idx);
-                
+
         return 0;
 }
 
 int main()
 {
-        bool icp = true;
+        bool icp = false;
         if (icp)
         {
                 return icp_accuracy_test();
@@ -125,7 +125,6 @@ int main()
 
                 VirtualSensor sensor;
                 sensor.init(Configuration::getDataSetPath());
-
                 float sigmaS(2.0);
                 float sigmaR(2.0);
                 std::cout << "Using sigmaS: " << sigmaS << std::endl;
@@ -165,9 +164,19 @@ int main()
                 int idx = 0;
                 Matrix4f trajectoryOffset;
                 auto totalComputeStart = std::chrono::high_resolution_clock::now();
+                std::vector<std::chrono::high_resolution_clock::duration> measurementsTsdfUpdate;
+                std::vector<std::chrono::high_resolution_clock::duration> measurementsPyramidCompute;
+                std::vector<std::chrono::high_resolution_clock::duration> measurementsRayCast;
+                std::vector<std::chrono::high_resolution_clock::duration> measurementsICP;
+                std::vector<std::chrono::high_resolution_clock::duration> measurementsFrame;
+                measurementsICP.reserve(800);
+                measurementsPyramidCompute.reserve(800);
+                measurementsRayCast.reserve(800);
+                measurementsTsdfUpdate.reserve(800);
                 while (sensor.processNextFrame())
                 {
                         auto frameComputeStart = std::chrono::high_resolution_clock::now();
+
                         float *depth = sensor.getDepth();
                         // Trajectory:       world -> view space (Extrinsics)
                         // InvTrajectory:    view -> world space (Pose)
@@ -186,21 +195,14 @@ int main()
                         grid.updateTSDF(sensor.getTrajectory() * trajectoryOffset, sensor.getDepthIntrinsics(), depth, sensor.getDepthImageWidth(), sensor.getDepthImageHeight());
 #if EVAL_MODE
                         auto updateTSDFEnd = std::chrono::high_resolution_clock::now();
-                        std::cout << "Computing the TSDF update (volumetric fusion) took: " << std::chrono::duration_cast<std::chrono::milliseconds>(updateTSDFEnd - updateTSDFStart).count() << " ms" << std::endl;
+                        measurementsTsdfUpdate.push_back(updateTSDFEnd - updateTSDFStart);
                         auto pyramidComputeStart = std::chrono::high_resolution_clock::now();
 #endif
                         PointCloudPyramid pyramid(sensor.getDepth(), sensor.getDepthIntrinsics(), sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), 2, windowSize, blockSize, sigmaR, sigmaS);
-                        /* Use this to compare the normals and scenes of pyramid and raycast
-                        ImageUtil::saveNormalMapToImage((float *) pyramid.getPointClouds().at(0).getNormalsCPU(), sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), std::string("PyramidImage_") + std::to_string(0), "");
-                        writeMesh(pyramid.getPointClouds().at(0).getPointsCPU(), sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), Configuration::getOutputDirectory() + std::string("mesh_") + std::to_string(0) + ".off");
-                        ImageUtil::saveNormalMapToImage((float *) pyramid.getPointClouds().at(1).getNormalsCPU(), sensor.getDepthImageWidth() >> 1, sensor.getDepthImageHeight() >> 1, std::string("PyramidImage_") + std::to_string(1), "");
-                        writeMesh(pyramid.getPointClouds().at(1).getPointsCPU(), sensor.getDepthImageWidth() >> 1, sensor.getDepthImageHeight() >> 1, Configuration::getOutputDirectory() + std::string("mesh_") + std::to_string(1) + ".off");
-                        ImageUtil::saveNormalMapToImage((float *) pyramid.getPointClouds().at(2).getNormalsCPU(), sensor.getDepthImageWidth()>> 2, sensor.getDepthImageHeight()>> 2, std::string("PyramidImage_") + std::to_string(2), "");
-                        writeMesh(pyramid.getPointClouds().at(2).getPointsCPU(), sensor.getDepthImageWidth() >> 2, sensor.getDepthImageHeight() >> 2, Configuration::getOutputDirectory() + std::string("mesh_") + std::to_string(2) + ".off");
-                        */
 #if EVAL_MODE
                         auto pyramidComputeEnd = std::chrono::high_resolution_clock::now();
-                        std::cout << "Computing the pyramid took: " << std::chrono::duration_cast<std::chrono::milliseconds>(pyramidComputeEnd - pyramidComputeStart).count() << " ms" << std::endl;
+                        measurementsPyramidCompute.push_back(pyramidComputeEnd - pyramidComputeStart);
+                        // std::cout << "Computing the pyramid took: " << std::chrono::duration_cast<std::chrono::milliseconds>(pyramidComputeEnd - pyramidComputeStart).count() << " ms" << std::endl;
 #endif
 #if SAVE_IMAGE_MODE
                         if (idx % 100 == 0)
@@ -210,7 +212,7 @@ int main()
                                 {
                                         std::cout << "Generating mesh for level " << i << std::endl;
                                         ImageUtil::saveNormalMapToImage((float *)pyramid.getPointClouds().at(i).getNormalsCPU(), sensor.getDepthImageWidth() >> i, sensor.getDepthImageHeight() >> i, std::string("PyramidImage_") + std::to_string(idx) + "_level" + std::to_string(i), "");
-                                        writeMesh(pyramid.getPointClouds().at(i).getPointsCPU(), sensor.getDepthImageWidth() >> i, sensor.getDepthImageHeight() >> i, Configuration::getOutputDirectory() + std::string("PyramidMesh_") + std::to_string(idx) + "_level" + std::to_string(i) + ".off");
+                                        //writeMesh(pyramid.getPointClouds().at(i).getPointsCPU(), sensor.getDepthImageWidth() >> i, sensor.getDepthImageHeight() >> i, Configuration::getOutputDirectory() + std::string("PyramidMesh_") + std::to_string(idx) + "_level" + std::to_string(i) + ".off");
                                 }
                         }
 #endif
@@ -218,32 +220,76 @@ int main()
                         auto raycastStart = std::chrono::high_resolution_clock::now();
 #endif
                         RaycastImage raycast = grid.raycastVoxelGrid(sensor.getTrajectory() * trajectoryOffset, sensor.getDepthIntrinsics());
-                        /* Use this to compare the normals and scenes of pyramid and raycast
-                        ImageUtil::saveNormalMapToImage((float *)raycast.m_normalMap, sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), std::string("RaycastedImage_") + std::to_string(idx), "");
-                        writeMesh(raycast.m_vertexMap, sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), Configuration::getOutputDirectory() + "mesh_" + std::to_string(idx) + ".off");
-                        */
 #if SAVE_IMAGE_MODE
-                        if (idx % 100 == 0)
-                        {
-                                ImageUtil::saveNormalMapToImage((float *)raycast.m_normalMap, sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), std::string("RaycastedImage_") + std::to_string(idx), "");
-                                writeMesh(raycast.m_vertexMap, sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), Configuration::getOutputDirectory() + "RaycastedMesh_" + std::to_string(idx) + ".off");
-                        }
+                        ImageUtil::saveNormalMapToImage((float *)raycast.m_normalMap, sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), std::string("RaycastedImage_") + std::to_string(idx), "");
+                        // writeMesh(raycast.m_vertexMap, sensor.getDepthImageWidth(), sensor.getDepthImageHeight(), Configuration::getOutputDirectory() + "RaycastedMesh_" + std::to_string(idx) + ".off");
+
 #endif
 #if EVAL_MODE
                         auto raycastStop = std::chrono::high_resolution_clock::now();
-                        std::cout << "Computing raycasting took: " << std::chrono::duration_cast<std::chrono::milliseconds>(raycastStop - raycastStart).count() << " ms" << std::endl;
+                        measurementsRayCast.push_back(raycastStop - raycastStart);
+                        // std::cout << "Computing raycasting took: " << std::chrono::duration_cast<std::chrono::milliseconds>(raycastStop - raycastStart).count() << " ms" << std::endl;
                         auto icpStart = std::chrono::high_resolution_clock::now();
 #endif
-                        prevFrameToGlobal = optimizer.optimize(pyramid, raycast.m_vertexMapGPU, raycast.m_normalMapGPU, prevFrameToGlobal);
+                        // prevFrameToGlobal = optimizer.optimize(pyramid, raycast.m_vertexMapGPU, raycast.m_normalMapGPU, prevFrameToGlobal);
 #if EVAL_MODE
                         auto icpEnd = std::chrono::high_resolution_clock::now();
+                        measurementsICP.push_back(icpEnd - icpStart);
                         auto frameComputeEnd = std::chrono::high_resolution_clock::now();
-                        std::cout << "Computing ICP took: " << std::chrono::duration_cast<std::chrono::milliseconds>(icpEnd - icpStart).count() << " ms" << std::endl;
-                        std::cout << "Computing the frame took: " << std::chrono::duration_cast<std::chrono::milliseconds>(frameComputeEnd - frameComputeStart).count() << " ms" << std::endl;
+                        measurementsFrame.push_back(frameComputeEnd - frameComputeStart);
 #endif
                 }
                 auto totalComputeStop = std::chrono::high_resolution_clock::now();
                 std::cout << "Computing for all frames took: " << std::chrono::duration_cast<std::chrono::milliseconds>(totalComputeStop - totalComputeStart).count() << " ms" << std::endl;
+#if EVAL_MODE
+                double meanTSDF = 0;
+                double meanPyramid = 0;
+                double meanRaycast = 0;
+                double meanICP = 0;
+                double meanFrame = 0;
+                for (size_t i = 0; i < measurementsTsdfUpdate.size(); i++)
+                {
+                        meanTSDF += std::chrono::duration_cast<std::chrono::milliseconds>(measurementsTsdfUpdate.at(i)).count();
+                        meanPyramid += std::chrono::duration_cast<std::chrono::milliseconds>(measurementsPyramidCompute.at(i)).count();
+                        meanRaycast += std::chrono::duration_cast<std::chrono::milliseconds>(measurementsRayCast.at(i)).count();
+                        meanICP += std::chrono::duration_cast<std::chrono::milliseconds>(measurementsICP.at(i)).count();
+                        meanFrame += std::chrono::duration_cast<std::chrono::milliseconds>(measurementsFrame.at(i)).count();
+                }
+                meanTSDF = meanTSDF / measurementsTsdfUpdate.size();
+                meanPyramid = meanPyramid / measurementsTsdfUpdate.size();
+                meanRaycast = meanRaycast / measurementsTsdfUpdate.size();
+                meanICP = meanICP / measurementsTsdfUpdate.size();
+                meanFrame = meanFrame / measurementsTsdfUpdate.size();
+                double varianceTSDF = 0;
+                double variancePyramid = 0;
+                double varianceRaycast = 0;
+                double varianceICP = 0;
+                double varianceFrame = 0;
+                for (size_t i = 0; i < measurementsTsdfUpdate.size(); i++)
+                {
+                        varianceTSDF += pow((meanTSDF - std::chrono::duration_cast<std::chrono::milliseconds>(measurementsTsdfUpdate.at(i)).count()), 2.0);
+                        variancePyramid += pow((meanPyramid - std::chrono::duration_cast<std::chrono::milliseconds>(measurementsPyramidCompute.at(i)).count()), 2.0);
+                        varianceRaycast += pow((meanRaycast - std::chrono::duration_cast<std::chrono::milliseconds>(measurementsRayCast.at(i)).count()), 2.0);
+                        varianceICP += pow((meanICP - std::chrono::duration_cast<std::chrono::milliseconds>(measurementsICP.at(i)).count()), 2.0);
+                        varianceFrame += pow((meanFrame - std::chrono::duration_cast<std::chrono::milliseconds>(measurementsFrame.at(i)).count()), 2.0);
+                }
+                varianceTSDF = varianceFrame / measurementsTsdfUpdate.size();
+                variancePyramid = variancePyramid / measurementsTsdfUpdate.size();
+                varianceRaycast = varianceRaycast / measurementsTsdfUpdate.size();
+                varianceICP = varianceICP / measurementsTsdfUpdate.size();
+                varianceFrame = varianceFrame / measurementsTsdfUpdate.size();
+
+                std::cout << "TSDF: Mean: " << meanTSDF << "ms"
+                          << " Variance: " << varianceTSDF << "ms" << std::endl;
+                std::cout << "Pyramid: Mean: " << meanPyramid << "ms"
+                          << " Variance: " << variancePyramid << "ms" << std::endl;
+                std::cout << "Raycast: Mean: " << meanRaycast << "ms"
+                          << " Variance: " << varianceRaycast << "ms" << std::endl;
+                std::cout << "ICP: Mean: " << meanICP << "ms"
+                          << " Variance: " << varianceICP << "ms" << std::endl;
+                std::cout << "Frame: Mean: " << meanFrame << "ms"
+                          << " Variance: " << varianceFrame << "ms" << std::endl;
+#endif
 #if SAVE_IMAGE_MODE
                 auto marchingCubesStart = std::chrono::high_resolution_clock::now();
                 run_marching_cubes(grid, idx);
